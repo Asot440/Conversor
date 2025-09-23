bfbb9e178188b52ffdfffcc596cec843f2e65f38
Adrian Martinez <asot_buuren440@hotmail.com>
2025-09-22 22:03:56 -0600

    Se agrega ejercicio 1 Calculadora IMC

--- 22092025/Codigo.js
+++ 22092025/Codigo.js
@@ -6,3 +6,11 @@ function convertidorMetrosKilometros(){
     //Asigna el valor de la operacion a la caja kilometros.
     document.getElementById("CajaKilometros").value=kilometros
 }
+
+function CalIMC(){
+    altura=parseFloat(document.getElementById("Altura").value);
+    peso=parseFloat(document.getElementById("Peso").value);
+    altura2=altura*altura;
+    imc=peso/altura2;
+    document.getElementById("IMC").value=imc
+}
