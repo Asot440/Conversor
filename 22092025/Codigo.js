51f9d99b24a1fe8006a786e3dc3d8c9f44ccbcbc
Jadri-PNZRKMPFWGN <asot_buuren440@hotmail.com>
2025-09-23 01:07:41 -0600

    Se agregan ejercicios 2 y 3

--- 22092025/Codigo.js
+++ 22092025/Codigo.js
@@ -14,3 +14,22 @@ function CalIMC(){
     imc=peso/altura2;
     document.getElementById("IMC").value=imc
 }
+
+function CelsFahr(){
+    celsius=parseFloat(document.getElementById("Celsius").value);
+    fahrenheit=(9/5)*celsius+32;
+    document.getElementById("Fahrenheit").value=fahrenheit
+}
+
+function Operaciones(){
+    a=parseFloat(document.getElementById("NumA").value)
+    b=parseFloat(document.getElementById("NumB").value)
+    suma=a+b;
+    document.getElementById("Suma").value=suma
+    resta=a-b;
+    document.getElementById("Resta").value=resta
+    multi=a*b;
+    document.getElementById("Multi").value=multi
+    div=a/b;
+    document.getElementById("Div").value=div
+}
