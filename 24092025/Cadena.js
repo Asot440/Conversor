cc1072def63cf26baace13abf4e33a4d7dc5d10f
JadriDeck <asot_buuren440@hotmail.com>
2025-09-28 20:33:13 -0600

    Se agregan ejercicios 4, 5 y 6

--- 24092025/Cadena.js
+++ 24092025/Cadena.js
@@ -6,3 +6,25 @@ function ConcatenarCadena(){
     document.getElementById("completo").value=completo;
     document.getElementById("posicion").value=completo.charAt(3);
 }
+
+function CalcularCadena(){
+    cadena=document.getElementById("cadena").value;
+    longitud=cadena.length;
+    document.getElementById("longitud").value=longitud
+}
+
+function MayusMinus(){
+    cadena=document.getElementById("cadena2").value;
+    mayus=cadena.toUpperCase();
+    minus=cadena.toLowerCase();
+    document.getElementById("Mayus").value=mayus;
+    document.getElementById("Minus").value=minus;
+}
+
+function FormatoFecha(){
+    dia=parseInt(document.getElementById("dia").value);
+    mes=parseInt(document.getElementById("mes").value);
+    año=parseInt(document.getElementById("año").value);
+    fecha=dia+"/"+mes+"/"+año;
+    document.getElementById("fecha").value=fecha;
+}
